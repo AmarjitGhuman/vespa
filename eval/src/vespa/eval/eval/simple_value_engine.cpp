0bff762bc18d6b81929300a2310dccc452864a25
Arne Juul <arnej@verizonmedia.com>
2020-10-18 18:43:35 +0000

    another engine

---
+++ eval/src/vespa/eval/eval/simple_value_engine.cpp
@@ -0,0 +1,117 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+#include "simple_value_engine.h"
+#include "value.h"
+#include "value_codec.h"
+#include <vespa/eval/instruction/generic_concat.h>
+#include <vespa/eval/instruction/generic_join.h>
+#include <vespa/eval/instruction/generic_map.h>
+#include <vespa/eval/instruction/generic_merge.h>
+#include <vespa/eval/instruction/generic_reduce.h>
+#include <vespa/eval/instruction/generic_rename.h>
+#include <vespa/eval/tensor/default_value_builder_factory.h>
+#include <vespa/vespalib/util/stringfmt.h>
+#include <cassert>
+
+using vespalib::tensor::DefaultValueBuilderFactory;
+using namespace vespalib::eval::instruction;
+
+namespace vespalib {
+namespace eval {
+
+const SimpleValueEngine SimpleValueEngine::_engine;
+
+//-----------------------------------------------------------------------------
+
+TensorSpec
+SimpleValueEngine::to_spec(const Value &value) const
+{
+    return spec_from_value(value);
+}
+
+Value::UP
+SimpleValueEngine::from_spec(const TensorSpec &spec) const
+{
+    return value_from_spec(spec, DefaultValueBuilderFactory::get());
+}
+
+//-----------------------------------------------------------------------------
+
+void
+SimpleValueEngine::encode(const Value &value, nbostream &output) const
+{
+    return encode_value(value, output);
+}
+
+Value::UP
+SimpleValueEngine::decode(nbostream &input) const
+{
+    return decode_value(input, DefaultValueBuilderFactory::get());
+}
+
+//-----------------------------------------------------------------------------
+
+const Value &
+SimpleValueEngine::map(const Value &a, map_fun_t function, Stash &stash) const
+{
+    if (a.is_double()) {
+        return stash.create<DoubleValue>(function(a.as_double()));
+    }
+    auto up = GenericMap::perform_map(a, function, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+const Value &
+SimpleValueEngine::join(const Value &a, const Value &b, join_fun_t function, Stash &stash) const
+{
+    if (a.is_double() && b.is_double()) {
+        return stash.create<DoubleValue>(function(a.as_double(), b.as_double()));
+    }
+    auto up = GenericJoin::perform_join(a, b, function, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+const Value &
+SimpleValueEngine::merge(const Value &a, const Value &b, join_fun_t function, Stash &stash) const
+{
+    auto up = GenericMerge::perform_merge(a, b, function, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+const Value &
+SimpleValueEngine::reduce(const Value &a, Aggr aggr, const std::vector<vespalib::string> &dimensions, Stash &stash) const
+{
+    auto up = GenericReduce::perform_reduce(a, aggr, dimensions, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+const Value &
+SimpleValueEngine::concat(const Value &a, const Value &b, const vespalib::string &dimension, Stash &stash) const
+{
+    auto up = GenericConcat::perform_concat(a, b, dimension, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+const Value &
+SimpleValueEngine::rename(const Value &a, const std::vector<vespalib::string> &from, const std::vector<vespalib::string> &to, Stash &stash) const
+{
+    auto up = GenericRename::perform_rename(a, from, to, DefaultValueBuilderFactory::get());
+    auto &result = stash.create<std::unique_ptr<Value>>(std::move(up));
+    const Value &result_ref = *(result.get());
+    return result_ref;
+}
+
+//-----------------------------------------------------------------------------
+
+} // namespace vespalib::eval
+} // namespace vespalib
